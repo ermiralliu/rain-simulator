a4d8bf6b1149b42c97d5862f91f814fbd5b3edc7
Ermir Alliu <mirialliu@gmail.com>
2025-06-02 22:10:22 +0200

    v1.7.3 -> before I start making everything local

--- UntitledUnmastered2.c
+++ UntitledUnmastered2.c
@@ -1,255 +1,243 @@
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <windows.h>
-#include <stdbool.h>
-#include <conio.h>
+#include "Supportive.h"
+#define X_DIMENSION 1400 //if you want to change the screen dimensions to another size
+#define Y_DIMENSION 800
+#define NrOf_RAINDROPS 3072 //if you want to make the raindrops denser or rarer
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer*renderer = 0;
-bool running = false;
+SDL_Renderer* renderer = 0;
+//bool running = false;
 
-SDL_Texture* m_pTexture; // the new SDL_Texture variable
-SDL_Rect srcR, destR; // source rectangle and where it should go
+SDL_Texture* m_pTexture[6]; //background texures
+SDL_Rect srcR, destR;
 
-SDL_Texture* raintext;
+SDL_Texture* raintext[3]; //rain textures/types
 SDL_Rect rainSrc;
-SDL_Rect rainDest[4608];
 
-int v[4608];
+Uint32 count=0;
+short int temperature= -1;
+RainType currentType = SNOW;
+int ground = 0, baseSpeed = 5; //ground is the background counter, baseSpeed is the vertical falling speed of the rain
+double wind = 0.0;
+unsigned long int reCnt = 0; //counter needed to change backgrounds;
 
-unsigned long int count=0;
+int main(int argc, char* argv[]){
+	showConsole(false);
+	Droplet drop[NrOf_RAINDROPS];
+	bool running = false;
+	if(init("Rain Simulator", X_DIMENSION, Y_DIMENSION, SDL_WINDOW_SHOWN| SDL_WINDOW_ALLOW_HIGHDPI, drop))
+		running = true;
+	else
+		return 1;
+	Uint32 frameStart;
+	int frameTime;
+	while(running){
+		frameStart = SDL_GetTicks();
+		running = event(drop);
+		update(drop);
+		render(drop);
+		frameTime = SDL_GetTicks()-frameStart;
+		if(FRAMEDELAY > frameTime)
+			SDL_Delay(FRAMEDELAY - frameTime);
+	}
+	SDL_Quit();
+	return 0;
+}
 
-HWND window;
+bool init(const char* title, int height, int width, int flags, Droplet *drop){	
+	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
+		SDL_Window* g_pWindow = SDL_CreateWindow(title,SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,	height, width, flags);
+		if(g_pWindow != 0){
+			renderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+			rain(drop);
+			background();
+		}
+	}
+	else 
+		return false;
+	return true;
+}
 
-int l=5;
-int temp= 1;
-short int w=0;
+void render(Droplet* drop){
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	if(temperature>80 && ground==0 )
+		SDL_RenderCopy(renderer, m_pTexture[5], &srcR, &destR);
+	else
+		SDL_RenderCopy(renderer, m_pTexture[ground], &srcR, &destR);
+	for(int a=0; a<NrOf_RAINDROPS; a++){
+		if(drop[a].type == STATICSNOW || drop[a].type == SNOW)
+			SDL_RenderCopy(renderer, raintext[SNOW], &rainSrc, &drop[a].dest);
+		else
+			SDL_RenderCopyEx(renderer, raintext[drop[a].type], &rainSrc, &drop[a].dest, atan(wind/baseSpeed)*180/M_PI, 0, SDL_FLIP_NONE);
+	}
+	SDL_RenderPresent(renderer);
+}
 
-const int FPS=60;
-const int frameDelay = 1000/FPS;
+void update(Droplet* drop){
+	for(int a=0 ; a<NrOf_RAINDROPS; a++){
+		if(drop[a].type != STATICSNOW){
+			if(drop[a].dest.y< Y_DIMENSION+30){
+				drop[a].dest.y += baseSpeed* drop[a].dest.h /20;
+				if(drop[a].dest.y >= Y_DIMENSION-20 && !drop[a].counted){ //Y_DIMENSION-20 cause the snow wont stick otherwise
+					reCnt++;
+					count++;
+					drop[a].counted =true;
+					if(currentType == SNOW){
+						if(rand()%32==0 && ground<4){
+							drop[a].type= STATICSNOW;
+							if(reCnt> 5000+ 5000*ground)
+								++ground;
+						}
+						else if(ground>=4 && rand()%32==0)
+							drop[a].type = STATICSNOW;
+					}	
+				}
+				if(currentType == SNOW)
+					drop[a].dest.x -= wind* drop[a].dest.h /20 -1 + (rand()%3);	
+				else if(currentType == RAIN){
+					drop[a].dest.x -= wind* drop[a].dest.h/20;
+					if(ground>0 &&  reCnt > 6000*(5-ground))
+						--ground;
+				}
+				if(drop[a].dest.x > X_DIMENSION)
+					drop[a].dest.x = 2;
+				else if(drop[a].dest.x < 1)
+					drop[a].dest.x = X_DIMENSION;
+			}
+			else{
+				drop[a].dest.y = rand()%5 -10;
+				drop[a].counted = false;
+				drop[a].type = currentType;
+			}
+		}
+		else if(temperature> 0 && (reCnt >= 80000 || rand()%512 == 0))
+			drop[a].type = currentType;
+	}
+}
 
-Uint32 frameStart;
-int frameTime;
+bool event(Droplet* drop){
+	SDL_Event e;
+	while(SDL_PollEvent(&e)){
+		if (e.type== SDL_QUIT)
+			return Quit();
+		else if(e.type == SDL_KEYDOWN) {
+			if(e.key.keysym.sym == SDLK_ESCAPE)
+				return Quit();
+			else if(e.key.keysym.sym == SDLK_w){
+					showConsole(true);
+					printf("Enter wind speed (pixel per second), positive values to the right:\n");
+					scanf("%lf",&wind);
+					wind/=-60;
+					exeToForeground();
+			}
+   			else if(e.key.keysym.sym == SDLK_t){
+				showConsole(true);
+				if(reCnt>3000)
+					reCnt=0;
+				printf("Enter temperature: (in degree Celsius)\n");
+				scanf("%d",&temperature);
+				arrange();
+				while(temperature> 200 || temperature< -274){
+					if(temperature> 200){
+						ground=0;
+						SDL_RenderClear(renderer);
+						SDL_RenderCopy(renderer, m_pTexture[5], &srcR, &destR);
+						SDL_RenderPresent(renderer);
+						printf("\nTOO HOT\nAll the rain has vaporized\nEnter another temperature (less than 200):\n");
+					}	
+					else if(temperature< -274){
+						SDL_RenderClear(renderer);
+						SDL_RenderPresent(renderer);
+						printf("\nUnder absolute 0\nNo Sun, No hope, Nothing\nReenter another temperature (more than -274):\n");
+					}
+					scanf("%d",&temperature);
+					if(temperature>=-274 && temperature <=200){
+						arrange();
+						placeDrops(drop);
+					}
+				}
+				exeToForeground();
+				
+			}
+			else if(e.key.keysym.sym == SDLK_r){
+				showConsole(true);
+				printf("Number of fallen raindrops fallen until now is: %d\n",count);
+				printf("Press Enter to continue:\n");
+				char a= getch();
+				exeToForeground();
+			}
+		}
+	}
+	return true;
+}
+//from here to the next comment: texture loaders
+void background(){
+	m_pTexture[0] = TextureLoader("assets/sky1.png", renderer);
+	m_pTexture[1] = TextureLoader("assets/sky2.png", renderer);
+	m_pTexture[2] = TextureLoader("assets/sky3.png", renderer);
+	m_pTexture[3] = TextureLoader("assets/sky4.png", renderer);
+	m_pTexture[4] = TextureLoader("assets/sky5.png", renderer); //getting progressively more snowy
+	m_pTexture[5] = TextureLoader("assets/skyHot.png", renderer); //5 is steamy
+	SDL_QueryTexture(m_pTexture[0], NULL, NULL, &srcR.w, &srcR.h);
+	destR.x = srcR.x = 0;
+	destR.y = srcR.y = 0;
+	destR.w = X_DIMENSION;
+	destR.h = Y_DIMENSION;
+}
+
+void rain(Droplet* drop){
+	raintext[BRESHER] = TextureLoader("assets/hail.png", renderer);
+	raintext[RAIN] = TextureLoader("assets/raindrop.png", renderer);
+	raintext[SNOW] = TextureLoader("assets/snow.png", renderer);
+	SDL_QueryTexture(raintext[RAIN], NULL, NULL, &rainSrc.w, &rainSrc.h);
+	rainSrc.x = 0;
+	rainSrc.y = 0;
+	placeDrops(drop);
+}
+// from here on, supportive functions to write less lines
+void placeDrops(Droplet* drop){
+	srand((unsigned)time(NULL));
+	for(int a=0; a<NrOf_RAINDROPS; a++){
+		drop[a].type = currentType;
+		drop[a].dest.x = 2+ rand()%(X_DIMENSION-2);
+		drop[a].dest.y = -Y_DIMENSION + rand()% Y_DIMENSION;
+		drop[a].dest.h = rainSrc.h + 3*(rand()%6);
+		drop[a].dest.w = rainSrc.w*( drop[a].dest.h / drop[a].dest.h );
+	}
+}
+
+void arrange(){
+	if(temperature>=0){
+		if(rand()%4 ==0 && temperature <10){
+			currentType = BRESHER;
+			baseSpeed=6;
+		}	
+		else{
+			currentType = RAIN;
+			baseSpeed=5;
+		}
+	}
+	else{
+		currentType = SNOW;
+		baseSpeed=4;
+	}	
+}
 
 SDL_Texture* TextureLoader(const char* texture, SDL_Renderer* ren){
 	SDL_Surface* tempSurface = IMG_Load(texture);
 	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, tempSurface);
 	SDL_FreeSurface(tempSurface);
-	
 	return tex;
 }
 
-int n=1;
- 
-void background(int n){
-	if(n==1){m_pTexture = TextureLoader("assets/sky1.png", renderer);}
-	if(n==2){m_pTexture = TextureLoader("assets/sky2.png", renderer);}
-	if(n==3){m_pTexture = TextureLoader("assets/sky3.png", renderer);}
-	if(n==4){m_pTexture = TextureLoader("assets/sky4.png", renderer);}
-	if(n==5){m_pTexture = TextureLoader("assets/sky5.png", renderer);}
-	SDL_QueryTexture(m_pTexture, NULL, NULL, &srcR.w, &srcR.h);
-	destR.x = srcR.x = 0;
-	destR.y = srcR.y = 0;
-	destR.w = srcR.w;
-	destR.h = srcR.h;
-}
-
-unsigned long int CountOnlySnow=0;
-
-void rain(){
-	srand((unsigned)time(NULL));
-	if(temp>0){
-		if(temp<10 && rand()%4==0){
-			raintext = TextureLoader("assets/hail.png", renderer);
-			l=6;
-		}
-		else{
-			raintext = TextureLoader("assets/raindrop.png", renderer);
-			l=5;
-			n=1;
-		}
-	}
-	else if(temp<=0){
-	raintext = TextureLoader("assets/snow.png", renderer);
-	l= 4;
-	CountOnlySnow=0;
-	}
-	SDL_QueryTexture(raintext, NULL, NULL, &rainSrc.w, &rainSrc.h);
-	rainSrc.x = 0;
-	rainSrc.y = 0;
-	for(int a=0; a<4864; a++){
-		if(rand()%2==1){
-			v[a]= 1;
-			rainDest[a].x = 2+ 10*(a%128);
-			rainDest[a].y = -700 + rand()%5 + 20*a/128;
-			rainDest[a].h = rainSrc.h + 6*(rand()%3);
-			rainDest[a].w = rainSrc.w*(rainDest[a].h/rainSrc.h);
-
-		}
-		else{v[a]= 0;}
-	}
-}
-
-bool init(const char* title, int height, int width, int flags){
-	
-	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
-		g_pWindow = SDL_CreateWindow(title,SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,	height, width, flags);
-		if(g_pWindow != 0){
-			renderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-			rain();
-			background(n);
-		}
-	}
-	else{
-		return false;
-	}
-	return true;
-}
-
-void render(){
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, m_pTexture, &srcR, &destR);
-	for(int a=0; a<4864; a++){
-		if(v[a]==1 || v[a]==2){
-		SDL_RenderCopyEx(renderer, raintext, &rainSrc, &rainDest[a], w, 0, SDL_FLIP_NONE);
-		}
-	}
-	SDL_RenderPresent(renderer);
-}
-
-int snowMetric=500;
-
-void update(){
-	for(int a=0 ; a<4864; a++){
-		if(v[a]==1){
-			if(rainDest[a].y< 750){
-				rainDest[a].y += l*rainDest[a].h/20;
-				if(temp<0){
-				rainDest[a].x -= (tan(w*3.14159/180) * l * rainDest[a].h / 20) -1 + (rand()%3);
-				}
-				else{
-				rainDest[a].x -= tan(w*3.14159/180)*l*rainDest[a].h/20;
-				}
-				if(rainDest[a].x>1280){
-						rainDest[a].x = 2;
-				}
-				else if(rainDest[a].x < 1){
-						rainDest[a].x = 1280;
-				}
-				if(rainDest[a].y >= 700 && rainDest[a].y < 705){
-					count++;
-					CountOnlySnow++;
-					if(temp<=0 && rand()%4==0){
-						v[a]=2;
-							if(CountOnlySnow>snowMetric && n<5){
-							background(++n);
-							snowMetric+=3500/n;
-							}	
-					}
-					if(temp<=0 && n>4){
-						v[a]=2;	
-					}
-
-				}
-			}
-			else{
-				rainDest[a].y = rand()%5 -10;
-				rainDest[a].x = 10*(a%128);
-
-			}
-		}
-	}
-}
-
-
-void showconsole(){
-	window = FindWindowA("ConsoleWindowClass", NULL);
-	ShowWindow(window, 1);
-}
-
-void exeToForeground(){
-	HWND hWnds = FindWindowA(NULL, "Rain Simulator" );
-	SetActiveWindow(hWnds);
-}
-
 void writeFile(){
-	FILE *fptr;
-	fptr = fopen("Raindrops.txt", "w");
-	fprintf(fptr, "Number of fallen raindrops in the end is:%d", count);
+	FILE *fptr = fopen("Raindrops.txt", "w");
+	fprintf(fptr, "Number of fallen raindrops is:%d\n", count);
 	fclose(fptr);
 }
 
-void event(){
-	SDL_Event e;
-	while(SDL_PollEvent(&e)){
-		if (e.type== SDL_QUIT){
-			showconsole();
-			ShowWindow(window, 1);
-			printf("Number of fallen raindrops in the end is: %d\n",count);
-			writeFile();
-			running=false;
-		}
-		else if(e.type == SDL_KEYDOWN) {
-			if(e.key.keysym.sym == SDLK_ESCAPE){
-					showconsole();
-					printf("Number of fallen raindrops in the end is: %d\n",count);
-					writeFile();
-					running=false;
-			}
-			else if(e.key.keysym.sym == SDLK_w){
-					showconsole();
-					printf("Enter wind speed (angle of the rain in clockwise direction):\n");
-					scanf("%d",&w);
-					ShowWindow(window, 0);
-					exeToForeground();
-			}
-   			else if(e.key.keysym.sym == SDLK_t){
-					showconsole();
-					printf("Enter temperature: (in degree Celsius)\n");
-					scanf("%d",&temp);
-					n=1;
-					background(1);
-					snowMetric=500;
-					CountOnlySnow=0;
-					ShowWindow(window, 0);
-					rain();
-					exeToForeground();
-			}
-			else if(e.key.keysym.sym == SDLK_r){
-					showconsole();
-					printf("Number of fallen raindrops fallen until now is: %d\n",count);
-					printf("Press any key to continue:\n");
-					getch();
-					ShowWindow(window, 0);
-					exeToForeground();
-			}
-		}
-	}
+bool Quit(){
+	showConsole(true);
+	writeFile();
+	printf("Number of fallen raindrops in the end is: %d\n", count);
+	return false;
 }
-
-int main(int argc, char* argv[]){
-	window = FindWindowA("ConsoleWindowClass", NULL);
-	ShowWindow(window, 0);
-	if(init("Rain Simulator", 1280, 720, SDL_WINDOW_SHOWN| SDL_WINDOW_ALLOW_HIGHDPI)){
-		running = true;
-	}
-	else{
-		return 1;
-	}
-	while(running){
-		frameStart = SDL_GetTicks();
-		event();
-		update();
-		render();
-		frameTime = SDL_GetTicks()-frameStart;
-		if(frameDelay > frameTime){
-			SDL_Delay(frameDelay - frameTime);
-		}
-	}
-	SDL_Quit();
-	return 0;
-}//*/
